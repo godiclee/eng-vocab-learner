16829342c1259d2d2bf6c818728220d108503d57
godiclee <b06901118@ntu.edu.tw>
2022-01-06 15:14:30 +0800

    oh

--- server.js
+++ server.js
@@ -13,18 +13,6 @@ app.use(bodyParser.json());
 import mongoose from 'mongoose';
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true,}).then((res) => console.log("mongo db connection created"));
 ""
-
-import path from "path";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-/* "nodemon server.js --ext js --exec babel-node", */
-/* "node server.js" */
-
 const port = process.env.PORT || 80;
 app.listen(port, () =>
   console.log(`Example app listening on port ${port}!`),
@@ -37,3 +25,14 @@ app.get("/new-card", (req, res) => {
 app.post("/add-card", (req, res) => {
   console.log(req.body.word);
 });
+
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+/* "nodemon server.js --ext js --exec babel-node", */
+/* "node server.js" */
