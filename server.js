9530a8211884ada3d4205f8921d9fa61269f6a87
godiclee <b06901118@ntu.edu.tw>
2022-01-06 13:20:17 +0800

    try after fixed before adding tutorial

--- server.js
+++ server.js
@@ -13,6 +13,16 @@ app.use(bodyParser.json());
 import mongoose from 'mongoose';
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true,}).then((res) => console.log("mongo db connection created"));
 ""
+/*
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+*/
 const port = process.env.PORT || 5000;
 app.listen(port, () =>
   console.log(`Example app listening on port ${port}!`),
