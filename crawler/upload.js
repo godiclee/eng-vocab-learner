b8f6c1b7adffeeaaddf6e6cd8196321184f68520
godic <31987572+godiclee@users.noreply.github.com>
2022-01-15 15:59:07 +0800

    Update upload.js

--- crawler/upload.js
+++ crawler/upload.js
@@ -3,7 +3,8 @@ import { strict as assert } from 'assert'
 
 
 import mongoose from 'mongoose';
-mongoose.connect("mongodb+srv://godiclee:292118tik@cluster0.mjnag.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true,}).then((res) => console.log("mongo db connection created"));
+# 需要上傳的話請在下方字串裏插入MONGO_URL
+mongoose.connect("INSERT_YOUR_OWN_MONGO_URL", {useNewUrlParser: true, useUnifiedTopology: true,}).then((res) => console.log("mongo db connection created"));
 console.log(data.length)
 import card from '../backend/models/card.js';
 
